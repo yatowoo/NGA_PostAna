64baf8c0d86c51569cfcf6733f4a517a83106ca3
ytwu <torrence@mail.ustc.edu.cn>
2018-11-29 02:28:24 +0800

    UPDATE check raw data info and ignore closed votes

--- run-analysis.py
+++ run-analysis.py
@@ -4,6 +4,10 @@
 
 import os
 import json
+import time
+
+def date2unix(str):
+  return int(time.mktime(time.strptime(str,"%Y-%m-%d %H:%M:%S")))
 
 metafile = open("metadata.json")
 metadata = json.load(metafile)
@@ -13,8 +17,23 @@ SAIMOE_YEAR = '2018'
 SAIMOE_STAGE = 'group_stage'
 
 for group,thread in metadata[SAIMOE_YEAR]['group_stage'].items():
-  print("[-] INFO - Start analysis for " + SAIMOE_YEAR + ' ' + SAIMOE_STAGE + ' ' + group)
+  print("\n---> Processing : " + SAIMOE_YEAR + ' ' + SAIMOE_STAGE + ' ' + group)
+
   postfile = 'output/NGA-' + repr(thread['tid']) + '.json'
+  try:
+    file_info = os.stat(postfile)
+  except FileNotFoundError:
+    print("[-] NEW post found in metadata.json")
+  else:
+    deadline = date2unix(thread['deadline'])
+    # Check file last modified time
+    if(file_info.st_mtime > deadline):
+      print("[-] INFO - Thread " + repr(thread['tid']) + " has already been latest version")
+      continue
+    else:
+      print("[-] INFO - Thread " + repr(thread['tid']) + " will be updated to latest version")
+
+  print("[-] INFO - Start analysis for " + SAIMOE_YEAR + ' ' + SAIMOE_STAGE + ' ' + group)
   csvfile = 'output/' + SAIMOE_YEAR + '-' + SAIMOE_STAGE + '-' + group + '.csv'
   # Get post by NGA tid
   cmd = './get-post.py '+ repr(thread['tid'])
