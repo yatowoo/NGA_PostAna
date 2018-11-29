a48d45ae553ee288daafc99b4c9077b759b5e1bf
ytwu <torrence@mail.ustc.edu.cn>
2018-11-29 22:24:46 +0800

    NEW output Excel formula to sum result in single page and total

--- ana-group.py
+++ ana-group.py
@@ -2,6 +2,7 @@
 
 # Script for NGA saimoe group analysis
 # ChangeLogs
+## v1.2: 直接输出excel公式统计单帖选择数、总计、分页统计
 ## v1.1: 支持繁体与中文名谐音
 ## v1: use external metadata for analysis
 ## v0: analysis output/18-GroupA.csv -> output/18-GroupA-result.csv
@@ -80,17 +81,19 @@ filename = os.path.basename(sys.argv[1]).split('.')[0]
 
 result_file = open(dirname +"/" + filename + "-result.csv","w")
 header = ['post_no','uid','reg_time','post_time','text','选择数'] + candidates
-writer = csv.DictWriter(result_file, fieldnames=header)
+writer = csv.DictWriter(result_file, fieldnames=header, extrasaction='ignore')
 writer.writeheader()
 
 data = []
-excel_row_no = 0 # 导入Excel后的行号
+excel_row_no = 1 # 导入Excel后的行号，第一行为标题
+excel_start_col = chr( ord('A') + len(header) - len(candidates))
+excel_end_col = chr( ord('A') + len(header) - 1)
 for row in raw:
   # 直接输出Excel公式，统计本行选择数
   # 求和公式格式：=SUM($G$2:$V$2)
-  # TODO: 若修改输出格式需修改公式，如增加页码列
   excel_row_no += 1
-  row['选择数'] = '=SUM($G$'+ repr(excel_row_no +1) + ':$V$' + repr(excel_row_no +1) + ')'
+  row['excel_row_no'] = excel_row_no
+  row['选择数'] = '=SUM($' + excel_start_col + '$'+ repr(excel_row_no) + ':$' + excel_end_col + '$' + repr(excel_row_no) + ')'
   for name in candidates:
     # 预处理中文繁体
     post_content = zhconv.convert(row['text'], 'zh-cn')
@@ -113,8 +116,49 @@ for row in raw:
   if(not pass_selection_num_check(row)):
     print(row['post_no']+'\t'+row['reg_time']+'\t'+row['text'])
   writer.writerow(row)
-
   data.append(row)
+# End of post analysis
+
+# Output excel formula to sum the result in total & pages
+def get_empty_row():
+  new_row = {}
+  for colName in header:
+    new_row[colName] = ''
+  return new_row
+
+# Excel公式 - 总和
+# 形式：=SUM($G$2:$G$800)
+fTotal = get_empty_row()
+fTotal['选择数'] = '总计'
+excel_col = {}
+for i,name in enumerate(candidates):
+  excel_col[name] = chr(ord(excel_start_col) + i)
+  fTotal[name] = "=SUM($" + excel_col[name] + "$2:$" + excel_col[name] + "$" + repr(excel_row_no) + ")"
+writer.writerow(fTotal)
+# 分页
+# 根据楼层post_no - 第一页[1, 19]，中间[20(n-1), 20n-1], 最后[20(n-1), -]
+# TODO: 考虑export-post.py时增加页码，尽管NGA删帖直接抽楼不改分页
+ROW_PER_PAGE = 20
+PAGE_TOTAL = int(int(data[-1]['post_no']) / ROW_PER_PAGE) + 1
+current_page = 1
+current_page_start_excel_row = 2
+for row in data:
+  if(int(row['post_no']) >= current_page * ROW_PER_PAGE):
+    fPage = get_empty_row()
+    fPage['选择数'] = current_page
+    for name in candidates:
+      fPage[name] = "=SUM($" + excel_col[name] + "$" + repr(current_page_start_excel_row) + ":$" + excel_col[name] + "$" + repr(row['excel_row_no'] -1) + ")"
+    writer.writerow(fPage)
+    current_page_start_excel_row = row['excel_row_no']
+    current_page +=1
+# 末页
+# TODO: 合并为write_page_formula()
+fPage = get_empty_row()
+fPage['选择数'] = current_page
+for name in candidates:
+  fPage[name] = "=SUM($" + excel_col[name] + "$" + repr(current_page_start_excel_row) + ":$" + excel_col[name] + "$" + repr(row['excel_row_no'] -1) + ")"
+writer.writerow(fPage)
+
 
 csvfile.close()
 result_file.close()
