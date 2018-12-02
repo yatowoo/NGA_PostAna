da0603a0b125d7fbd19ed06929ec71ffd20a782b
ytwu <torrence@mail.ustc.edu.cn>
2018-12-03 01:27:08 +0800

    NEW column for validation in result file and also output validation failed row in validation.csv

--- ana-group.py
+++ ana-group.py
@@ -59,9 +59,54 @@ def pass_selection_num_check(row):
   for name in candidates:
     if(row[name]):
       selection_num += 1
+  row['Nselection'] = selection_num
   if(selection_num > SELECTION_MAX):
     return False
   return True
+
+# Validate result with number of splitted words
+def pass_validation(row):
+  text = row['text']
+  text = text.lower()
+  selection_num = 0
+  # Remove <BLANK> in name to avoid being splitted
+  for name in candidates:
+    if(row[name]):
+      selection_num += 1
+      if(name == '欧根'):
+        text = text.replace('prinz eugen', 'PrinzEugen')
+      elif(name == '甘比尔湾'):
+        text = text.replace('gambier bay', 'GambierBay')
+      elif(name == '皇家方舟'):
+        text = text.replace('ark royal', 'ArkRoyal')
+      elif(name == '路易吉'):
+        text = text.replace('luigi torelli', 'LuigiTorelli')
+      elif(name == '特斯特'):
+        text = text.replace('commandant teste', 'CommandantTeste')
+      elif(name == '齐柏林'):
+        text = text.replace('graf zeppelin', 'GrafZeppelin')
+  # Remove useless characters in post content
+  text = text.replace('zsbd','')
+  text = text.replace('紫薯布丁','')
+  text = text.replace('字数补丁','')
+  text = text.replace('字数','')
+  # Replace common delimiter with standard delimiter
+  text = text.replace('。', '|')
+  text = text.replace('，', '|')
+  text = text.replace('、', '|')
+  text = text.replace('<br/>','|')
+  text = text.replace(' ','|')
+  # Remove same entry in splitted list
+  word_set = set(text.split('|'))
+  if({''}.issubset(word_set)):
+    word_set.remove('')
+  # Validate matching result
+  word_num = len(word_set)
+  row['Nword'] = word_num
+  if(selection_num != word_num):
+    return False
+  else:
+    return True
 
 def alias_match(post_content, name):
   for alias in aliasDB[name]:
@@ -84,10 +129,11 @@ dirname = os.path.dirname(sys.argv[1])
 filename = os.path.basename(sys.argv[1]).split('.')[0]
 
 result_file = open(dirname +"/" + filename + "-result.csv","w")
+validation_file = open(dirname +"/" + filename + "-validation.csv","w")
 logfile = open('analysis.log','a+')
 logfile.write('\n------> Analysis log for NGA舰萌'+SAIMOE_YEAR+' '+SAIMOE_STAGE+' '+SAIMOE_GROUP+'组\n')
 logfile.write('Date : '+ time.strftime("%Y-%m-%d %H:%M:%S") + '\n')
-header = ['post_no','uid','reg_time','post_time','text','选择数'] + candidates
+header = ['post_no','uid','reg_time','post_time','text', '验证', '选择数'] + candidates
 writer = csv.DictWriter(result_file, fieldnames=header, extrasaction='ignore', dialect="excel-tab")
 writer.writeheader()
 
@@ -101,6 +147,7 @@ for row in raw:
   excel_row_no += 1
   row['excel_row_no'] = excel_row_no
   row['选择数'] = '=SUM($' + excel_start_col + '$'+ repr(excel_row_no) + ':$' + excel_end_col + '$' + repr(excel_row_no) + ')'
+  # 候选舰娘依次匹配
   for name in candidates:
     # 预处理中文繁体
     post_content = zhconv.convert(row['text'], 'zh-cn')
@@ -121,13 +168,26 @@ for row in raw:
     else:
       row[name] = ''
       MATCH_COUNT['总计'] -= 1
+  # 检查匹配结果
+    # 检查注册时间
   if(not pass_register_time_check(row)):
     # 清空结果，但仍输出回帖内容，表中注意标红
     for name in candidates:
       row[name] = ''
     logfile.write('新号\t'+row['post_no']+'\t'+row['reg_time']+'\t'+row['text']+'\n')
-  if(not pass_selection_num_check(row)):
-    logfile.write('超票\t'+row['post_no']+'\t'+row['text']+'\n')
+    row['验证'] = '-'
+  else:
+    # 检查选择舰娘数
+    if(not pass_selection_num_check(row)):
+      logfile.write('超票\t'+row['post_no']+'\t'+row['text']+'\n')
+      row['验证'] = '?'
+    # 根据分词进行验证
+    elif(pass_validation(row)):
+      row['验证'] = '●'
+    # 验证失败则导出以供分析
+    else:
+      row['验证'] = '×'
+      validation_file.write(row['post_no']+'\t'+row['text']+'\t'+repr(row['Nselection'])+'\t'+repr(row['Nword'])+'\n')
   writer.writerow(row)
   data.append(row)
 MATCH_COUNT['总楼层'] = len(data)
@@ -180,4 +240,5 @@ writer.writerow(fPage)
 csvfile.close()
 result_file.close()
 logfile.close()
+validation_file.close()
 
