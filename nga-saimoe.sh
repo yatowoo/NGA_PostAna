5436c0c32185d6a46c72cf62dc7d0b54c7d8945a
ytwu <torrence@mail.ustc.edu.cn>
2018-12-21 15:15:18 +0800

    FIX output

--- nga-saimoe.sh
+++ nga-saimoe.sh
@@ -10,9 +10,9 @@ cd $CODE_DIR
 git pull
 rm /etc/cron.hourly/$SCRIPT_NAME
 cp $SCRIPT_NAME /etc/cron.hourly/$SCRIPT_NAME
-cat run.log >> run_history.log
 echo > run.log
 ./run-analysis.py --info 2018 group64 1>run.out 2>run.err
+cat run.log >> run_history.log
 mkdir -p $WEB_DIR 
 cp run.* $WEB_DIR/
 # Validation file
