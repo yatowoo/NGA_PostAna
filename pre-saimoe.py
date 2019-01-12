3e281513bf9656ceb806c94f3432e4ce1aa8a24c
ytwu <torrence@mail.ustc.edu.cn>
2019-01-13 00:44:46 +0800

    NEW meta data for 2015

--- pre-saimoe.py
+++ pre-saimoe.py
@@ -1,18 +1,26 @@
-for i in range(16,21):
+for i in range(50,55):
   thread = tlist[i].split('\t')
-  gname = chr(ord('A')+i-16)
+  gname = '2' + chr(ord('A')+i-50)
   meta[gname] = {}
   meta[gname]['tid'] = int(thread[1])
-  meta[gname]['selection_max'] = 3
-  meta[gname]['deadline'] = "2015-02-14 20:00:00"
+  meta[gname]['selection_max'] = 1
+  meta[gname]['deadline'] = "2016-02-01 00:00:00"
   tfile = 'output/NGA-' + thread[1] + '.json'
   f = open(tfile)
   raw = json.load(f)
   f.close()
   head = raw[0]['data']['__R']['0']['content']
-  cdd = head[head.find('半决赛'):]
+  cdd = re.search('\[quote\](.*?)\[/quote\]',head,re.S).groups()[0]
   cdd = re.sub('\[img\].*?\[/img\]','',cdd,re.S)
-  meta[gname]['candidates'] = cdd.replace('<br/>',' ').split()[1:]
+  meta[gname]['candidates'] = cdd.replace('<br/>',' ').split()
+
+for gname in sorted(meta.keys()):
+  g = meta[gname]
+  print('"'+gname+'":{')
+  print('  "tid": '+repr(g['tid'])+',')
+  print('  "candidates": '+repr(g['candidates']).replace("'",'"')+',')
+  print('  "selection_max": '+repr(g['selection_max'])+',')
+  print('  "deadline": "'+g['deadline']+'"\n},')
 
 # Candidate list
   # 2014 group_stage
