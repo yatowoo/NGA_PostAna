b71c59d16f10798597d9a60f1307f31b88cf23be
ytwu <torrence@mail.ustc.edu.cn>
2019-05-10 22:33:50 +0800

    Test reply spilliting - 尝试中文分词

--- post-split.py
+++ post-split.py
@@ -5,59 +5,38 @@
 import csv
 import json
 import sys
+import re
+import jieba
+import pypinyin
 
-f = open('metadata.json')
-meta = json.load(f)
-f.close()
+csvfile = open('output/test.csv')
+raw = csv.DictReader(csvfile)
 
-# Read from metadata with arguments
-SAIMOE_YEAR = sys.argv[1]
-SAIMOE_STAGE = sys.argv[2]
-SAIMOE_GROUP = sys.argv[3]
+MEANINGLESS_WORD = ['zsbd', 'zs', '字数补丁', '字数布丁', '紫薯布丁', '字数', '紫薯', '补丁', "补字数", 'sgnb', '冲鸭', 'exe', 'jpg', 'txt', '单票', '单投', "_\(:з&#39;∠\)_", "&#92;"]
+NGA_TAG = ['quote', 'collapse', 'img', 'del', 'url', 'dice', 'code']
+def trim_content(text, delimiter='|'):
+  # Remove NGA tags
+  text = re.sub('\[s:.*?\]', '', text)
+  text = re.sub('\[/?b\]','',text)
+  text = re.sub('\[/?size.*?\]','',text)
+  for tag in NGA_TAG:
+    text = re.sub('\['+tag+'.*?\].*?\[/'+tag+'\]', '', text)
 
-candidates = meta[SAIMOE_YEAR][SAIMOE_STAGE][SAIMOE_GROUP]['candidates']
+  # Remove meaningless words / characters in post content
+  for w in MEANINGLESS_WORD:
+    text = re.sub(w, '', text, flags=re.I)
+  text = re.sub('[~`@#$%^…&*()（）_\-+=·——]', '', text)
 
-csvfile = open('output/'+ SAIMOE_YEAR + '-' + SAIMOE_STAGE + '-' + SAIMOE_GROUP +'-result.csv')
-raw = csv.DictReader(csvfile, dialect="excel-tab")
+  # Replace delimiter (if for match, remove them)
+  text = re.sub('[?？!！:：;；.。,，、　 ]', delimiter, text)
+  text = text.replace('<br/>',delimiter)
+  text = text.replace('&amp;',delimiter)
+  return text
 
-post = []
-selection = []
+jieba.load_userdict('alias.dict')
 for row in raw:
-  if(not row['post_no'].isdigit()):
-    continue
-  text = row['text']
-  text = text.lower()
-  selection_num = 0
-  for name in candidates:
-    if(row[name] == '1'):
-      selection_num += 1
-      if(name == '欧根'):
-        text = text.replace('prinz eugen', 'PrinzEugen')
-      elif(name == '甘比尔湾'):
-        text = text.replace('gambier bay', 'GambierBay')
-      elif(name == '皇家方舟'):
-        text = text.replace('ark royal', 'ArkRoyal')
-      elif(name == '路易吉'):
-        text = text.replace('luigi torelli', 'LuigiTorelli')
-      elif(name == '特斯特'):
-        text = text.replace('commandant teste', 'CommandantTeste')
-      elif(name == '齐柏林'):
-        text = text.replace('graf zeppelin', 'GrafZeppelin')
-  text = text.replace('zsbd','')
-  text = text.replace('紫薯布丁','')
-  text = text.replace('字数补丁','')
-  text = text.replace('字数','')
-  text = text.replace('。', '|')
-  text = text.replace('，', '|')
-  text = text.replace('、', '|')
-  text = text.replace('<br/>','|')
-  text = text.replace(' ','|')
-  post.append(text)
-  word_set = set(text.split('|'))
-  if({''}.issubset(word_set)):
-    word_set.remove('')
-  word_num = len(word_set)
-  if(selection_num != word_num):
-    print(row['post_no']+'\t'+row['text']+'\t'+repr(selection_num)+'\t'+repr(word_num))
+  text = trim_content(row['回帖内容'],'')
+  print('|'.join(jieba.cut(text)))
+
 
 csvfile.close()
